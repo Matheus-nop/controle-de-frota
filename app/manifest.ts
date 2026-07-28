47485483151d33e785ccfd0e4d5db515c4d4936d
Matheus-nop <matheus@novaopcaoequipamentos.com.br>
2026-07-28 11:48:39 -0300

    Fix icon file names in app manifest
    Updated icon file names in the manifest.

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,9 +11,9 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#EBEEF4",
     theme_color: "#17263F",
     icons: [
-      { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
-      { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
-      { src: "/icon-512.png", sizes: "512x512", type: "image/png", purpose: "maskable" },
+      { src: "/icon192.png", sizes: "192x192", type: "image/png" },
+      { src: "/icon512.png", sizes: "512x512", type: "image/png" },
+      { src: "/icon512.png", sizes: "512x512", type: "image/png", purpose: "maskable" },
     ],
   };
 }
